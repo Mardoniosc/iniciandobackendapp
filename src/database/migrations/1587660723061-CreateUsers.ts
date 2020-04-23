cee3a2ff03d6811847ebd0cffa5f2fb4988d8a59
Mardonio Costa <mardonio@live.com>
2020-04-23 19:46:48 -0300

    :v: Relacionanmento nos models

--- src/database/migrations/1587660723061-CreateUsers.ts
+++ src/database/migrations/1587660723061-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1587660723061 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
